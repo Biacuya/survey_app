6bed5bda1bd46ed440a4bf38b2d354232de7ec9f
Erik Molina <90419030+Biacuya@users.noreply.github.com>
2023-10-09 22:54:49 -0500

    Creation of a mianPage of survey

---
+++ src/app/components/surveyapp/surveyapp.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { Model } from "survey-core";
+
+const surveyJson = {
+  "title": "¡Bienvenido!",
+  "logoPosition": "right",
+  "pages": [
+   {
+    "name": "page1",
+    "elements": [
+     {
+      "type": "text",
+      "name": "Nombre",
+      "isRequired": true,
+      "autocomplete": "name"
+     },
+     {
+      "type": "text",
+      "name": "Apellido",
+      "isRequired": true
+     },
+     {
+      "type": "text",
+      "name": "Titulo de la encuesta",
+      "isRequired": true
+     },
+     {
+      "type": "checkbox",
+      "name": "Componentes de la encuesta",
+      "isRequired": true,
+      "choices": [
+       "Caja de texto",
+       "CheckBox",
+       "RadioButtons"
+      ],
+      "choicesOrder": "asc",
+      "showSelectAllItem": true,
+      "maxSelectedChoices": 10,
+      "minSelectedChoices": 1,
+      "selectAllText": "Seleccionar Todo"
+     }
+    ]
+   }
+  ],
+  "mode": "display"
+ }
+
+
+
+
+@Component({
+  selector: 'app-surveyapp',
+  templateUrl: './surveyapp.component.html',
+  styleUrls: ['./surveyapp.component.css']
+})
+export class SurveyappComponent implements OnInit {
+  surveyModel!: Model;
+  ngOnInit() {
+    const survey = new Model(surveyJson);
+    this.surveyModel = survey;
+  }
+}
