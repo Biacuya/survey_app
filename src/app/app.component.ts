55e458b446ba92f9dc7db70af49788bb835ee47a
Carlos <carlosmapache1@gmail.com>
2023-10-09 20:31:44 -0500

    Primera encuesta creada
    Se inicializa el esquema de la primera encuesta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Model } from "survey-core";
+
+
+const surveyJson = {
+  elements: [{
+    name: "FirstName",
+    title: "Ingresa el tipo de pregunta:",
+    type: "text"
+  }, {
+    name: "LastName",
+    title: "Ingresa tu ultimo nombre:",
+    type: "text"
+  }]
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'survey_app';
+export class AppComponent implements OnInit {
+  title = 'My First Survey';
+  surveyModel: Model = new Model(surveyJson);
+  ngOnInit() {
+    const survey = new Model(surveyJson);
+    this.surveyModel = survey;
+  }
 }
